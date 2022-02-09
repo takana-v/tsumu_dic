eefc4cc97efb80b1af2e70193810af2d599f78fe
takana-v <44311840+takana-v@users.noreply.github.com>
2022-02-10 03:05:59 +0900

    システム辞書用ビルド追加

--- test_pyopenjtalk_build.py
+++ test_pyopenjtalk_build.py
@@ -2,7 +2,9 @@ from pathlib import Path
 
 from pyopenjtalk import create_user_dict
 
-for dict_path in Path("dict").iterdir():
-    if dict_path.suffix != ".csv":
-        continue
-    create_user_dict(str(dict_path), str(dict_path)+".dic")
+for dict_dir_path in Path("dict").iterdir():
+    if dict_dir_path.is_dir:
+        for dict_path in dict_dir_path.iterdir():
+            if dict_path.suffix != ".csv":
+                continue
+            create_user_dict(str(dict_path), str(dict_path)+".dic")
